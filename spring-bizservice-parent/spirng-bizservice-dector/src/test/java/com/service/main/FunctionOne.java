288acb7bdebd518b773185c6e9fc23f54a6cbbbf
liyongzhen <liyongzhen@zhidianlife.com>
2018-08-22 09:03:04 +0800

    OK

---
+++ spring-bizservice-parent/spirng-bizservice-dector/src/test/java/com/service/main/FunctionOne.java
@@ -0,0 +1,13 @@
+package com.service.main;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+
+/**
+ * @author liyongzhen
+ * @create 2018-08-22 9:01
+ **/
+public class FunctionOne {
+    public static void main(String[] args) {
+        System.out.println("杨幂");
+    }
+}
